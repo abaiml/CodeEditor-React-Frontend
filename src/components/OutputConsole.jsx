cee5dc75496adfc0252101b670f8afcc1980b1b3
Ayush Bora <ayushbora1001@gmail.com>
2025-05-17 00:44:50 +0530

    Update OutputConsole.jsx

--- src/components/OutputConsole.jsx
+++ src/components/OutputConsole.jsx
@@ -1,8 +1,13 @@
 export default function OutputConsole({ output, isDarkMode }) {
   return (
-    <div className={`h-full w-full ${isDarkMode ? 'bg-black text-green-400' : 'bg-white text-green-800'} p-2 mr-10 font-mono overflow-auto border rounded`}>
-      <pre>{output}</pre>
+    <div
+      className={`
+        w-full max-h-full overflow-auto
+        ${isDarkMode ? "bg-black text-green-400" : "bg-white text-green-800"}
+        p-2 font-mono border rounded
+      `}
+    >
+      <pre className="whitespace-pre-wrap">{output}</pre>
     </div>
   );
 }
-
